aaedfb7b316525f6c55c79c489a4962c6768fe8c
MacmeDan <pdanielleonard@gmail.com>
2017-03-07 19:17:34 -0700

    Made 3 chats and made the view scrollable.

--- app/src/main/java/macemdan/chartdemo/MainActivity.java
+++ app/src/main/java/macemdan/chartdemo/MainActivity.java
@@ -1,45 +1,59 @@
 package macemdan.chartdemo;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    //Creating an Example Bar Chart.
-    private BarChart mChart;
+    //Creating Example Charts.
+    private BarChart mBarChart;
+    private PieChart mPieChart;
+    private LineChart mLineChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mChart = (BarChart) findViewById(R.id.chart);
 
-        mChart.setDrawGridBackground(false);
-        mChart.setDrawBarShadow(true);
-        mChart.setDrawValueAboveBar(true);
-
-        // if more than 60 entries are displayed in the chart, no values will be
-        // drawn
-        mChart.setMaxVisibleValueCount(60);
-
-        // scaling can now only be done on x- and y-axis separately
-        mChart.setPinchZoom(false);
+        mBarChart = (BarChart) findViewById(R.id.chart);
+        mPieChart = (PieChart) findViewById(R.id.pieChart);
+        mLineChart = (LineChart) findViewById(R.id.LineChart);
 
         //Setting random data for demo purposes. \
         setData(17, 56);
+        mLineChart.setData(generateDataLine(3));
+        mPieChart.setData(generateDataPie(5));
 
     }
 
     private void setData(int count, float range) {
+        mBarChart.setDrawGridBackground(false);
+        mBarChart.setDrawBarShadow(true);
+        mBarChart.setDrawValueAboveBar(true);
+        // if more than 60 entries are displayed in the chart, no values will be
+        // drawn
+        mBarChart.setMaxVisibleValueCount(60);
 
+        // scaling can now only be done on x- and y-axis separately
+        mBarChart.setPinchZoom(false);
         float start = 1f;
 
         ArrayList<BarEntry> yVals1 = new ArrayList<BarEntry>();
@@ -57,12 +71,12 @@ public class MainActivity extends AppCompatActivity {
 
         BarDataSet set1;
 
-        if (mChart.getData() != null &&
-                mChart.getData().getDataSetCount() > 0) {
-            set1 = (BarDataSet) mChart.getData().getDataSetByIndex(0);
+        if (mBarChart.getData() != null &&
+                mBarChart.getData().getDataSetCount() > 0) {
+            set1 = (BarDataSet) mBarChart.getData().getDataSetByIndex(0);
             set1.setValues(yVals1);
-            mChart.getData().notifyDataChanged();
-            mChart.notifyDataSetChanged();
+            mBarChart.getData().notifyDataChanged();
+            mBarChart.notifyDataSetChanged();
         } else {
             set1 = new BarDataSet(yVals1, "The year 2017");
 
@@ -75,7 +89,92 @@ public class MainActivity extends AppCompatActivity {
             data.setValueTextSize(10f);
             data.setBarWidth(0.9f);
 
-            mChart.setData(data);
+            mBarChart.setData(data);
         }
+    }
+    /**
+     * generates a random ChartData object with just one DataSet
+     *
+     * @return
+     */
+    private LineData generateDataLine(int cnt) {
+
+        ArrayList<Entry> e1 = new ArrayList<Entry>();
+
+        for (int i = 0; i < 12; i++) {
+            e1.add(new Entry(i, (int) (Math.random() * 65) + 40));
+        }
+
+        LineDataSet d1 = new LineDataSet(e1, "New DataSet " + cnt + ", (1)");
+        d1.setLineWidth(2.5f);
+        d1.setCircleRadius(4.5f);
+        d1.setHighLightColor(Color.rgb(244, 117, 117));
+        d1.setDrawValues(false);
+
+        ArrayList<Entry> e2 = new ArrayList<Entry>();
+
+        for (int i = 0; i < 12; i++) {
+            e2.add(new Entry(i, e1.get(i).getY() - 30));
+        }
+
+        LineDataSet d2 = new LineDataSet(e2, "New DataSet " + cnt + ", (2)");
+        d2.setLineWidth(2.5f);
+        d2.setCircleRadius(4.5f);
+        d2.setHighLightColor(Color.rgb(244, 117, 117));
+        d2.setColor(ColorTemplate.VORDIPLOM_COLORS[0]);
+        d2.setCircleColor(ColorTemplate.VORDIPLOM_COLORS[0]);
+        d2.setDrawValues(false);
+
+        ArrayList<ILineDataSet> sets = new ArrayList<ILineDataSet>();
+        sets.add(d1);
+        sets.add(d2);
+
+        LineData cd = new LineData(sets);
+        return cd;
+    }
+
+    /**
+     * generates a random ChartData object with just one DataSet
+     *
+     * @return
+     */
+    private BarData generateDataBar(int cnt) {
+
+        ArrayList<BarEntry> entries = new ArrayList<BarEntry>();
+
+        for (int i = 0; i < 12; i++) {
+            entries.add(new BarEntry(i, (int) (Math.random() * 70) + 30));
+        }
+
+        BarDataSet d = new BarDataSet(entries, "New DataSet " + cnt);
+        d.setColors(ColorTemplate.VORDIPLOM_COLORS);
+        d.setHighLightAlpha(255);
+
+        BarData cd = new BarData(d);
+        cd.setBarWidth(0.9f);
+        return cd;
+    }
+
+    /**
+     * generates a random ChartData object with just one DataSet
+     *
+     * @return
+     */
+    private PieData generateDataPie(int cnt) {
+
+        ArrayList<PieEntry> entries = new ArrayList<PieEntry>();
+
+        for (int i = 0; i < 4; i++) {
+            entries.add(new PieEntry((float) ((Math.random() * 70) + 30), "Quarter " + (i+1)));
+        }
+
+        PieDataSet d = new PieDataSet(entries, "");
+
+        // space between slices
+        d.setSliceSpace(2f);
+        d.setColors(ColorTemplate.VORDIPLOM_COLORS);
+
+        PieData cd = new PieData(d);
+        return cd;
     }
 }
